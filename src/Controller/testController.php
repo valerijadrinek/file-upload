b4e1557dd7e965dcc945a9989be5485c6a322bb8
valerijadrinek <valerijadrinek@gmail.com>
2023-11-19 14:09:11 +0100

    initial upload

---
+++ src/Controller/testController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name:'test_upload')]
+    public function testUpload(Request $request) : Response
+    {
+    }
+}
