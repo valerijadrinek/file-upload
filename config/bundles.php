07d4cdb793111d266fc20fd79683abfb2af3f8d3
valerijadrinek <valerijadrinek@gmail.com>
2023-11-24 21:00:49 +0100

    flysystem

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
